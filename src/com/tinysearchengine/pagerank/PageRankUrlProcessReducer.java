31e09003e0a21f53fd66104f27911ef3588c2ade
Xueyin <xueyinw@seas.upenn.edu>
2017-04-24 13:56:59 -0400

    step1: initialization, need to consider crawler input format and make changes

---
+++ src/com/tinysearchengine/pagerank/PageRankUrlProcessReducer.java
@@ -0,0 +1,27 @@
+package com.tinysearchengine.pagerank;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.Reducer.Context;
+
+public class PageRankUrlProcessReducer extends Reducer<Text,Text,Text,Text> {
+	@Override 
+	public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+		StringBuffer pointToCurLink = new StringBuffer();
+		boolean isValid = false;
+		for(Text value : values) { //v has two situation: 1. isvalid; 2. links that points to this key
+			String curValue = value.toString();
+			if(curValue.equalsIgnoreCase("isValid")) {
+				isValid = true;
+				context.write(key, new Text("isValid"));
+			}
+			else pointToCurLink.append(" " + curValue);
+		}
+		int pointersLength = pointToCurLink.length();
+		if(pointersLength != 0 && isValid == true) {		
+			context.write(key,new Text(pointToCurLink.toString().substring(1, pointToCurLink.length())));
+		}
+	}
+}
