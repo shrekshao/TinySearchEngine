aa595a0533bd141653358760d99f475480244878
hengchu <hengchu.zhang@gmail.com>
2017-04-25 12:03:23 -0400

    Crawler: implemented async io

--- src/com/tinysearchengine/crawler/Crawler.java
+++ src/com/tinysearchengine/crawler/Crawler.java
@@ -10,22 +10,25 @@ import java.util.Date;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
+import java.util.function.Consumer;
 
 import org.apache.http.Header;
 import org.apache.http.HeaderElement;
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
-import org.apache.http.client.HttpClient;
 import org.apache.http.client.config.CookieSpecs;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpHead;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.apache.http.impl.nio.client.HttpAsyncClients;
+import org.apache.http.impl.nio.conn.PoolingNHttpClientConnectionManager;
+import org.apache.http.impl.nio.reactor.DefaultConnectingIOReactor;
+import org.apache.http.nio.client.HttpAsyncClient;
+import org.apache.http.nio.reactor.ConnectingIOReactor;
 import org.apache.http.nio.reactor.IOReactorException;
 import org.apache.http.util.EntityUtils;
 import org.apache.log4j.Logger;
+import org.apache.http.concurrent.FutureCallback;
 
 import com.amazonaws.services.dynamodbv2.datamodeling.S3Link;
 import com.googlecode.concurrentlinkedhashmap.ConcurrentLinkedHashMap;
@@ -62,8 +65,9 @@ public class Crawler {
 	private DdbConnector d_ddbConnector = new DdbConnector();
 	private DocumentFingerprintCache d_contentSeenCache =
 		new DocumentFingerprintCache(d_ddbConnector);
-	private PoolingHttpClientConnectionManager m_manager = null;
-	private HttpClient m_client = null;
+
+	private PoolingNHttpClientConnectionManager m_asyncManager = null;
+	private HttpAsyncClient m_asyncClient = null;
 
 	/**
 	 * @param dbDir
@@ -90,9 +94,10 @@ public class Crawler {
 		m_LRUdue = builder.concurrencyLevel(nThread).initialCapacity(1000)
 				.maximumWeightedCapacity(k_MAX_DUE_SIZE).build();
 
-		m_manager = new PoolingHttpClientConnectionManager();
-		m_manager.setMaxTotal(20 * nThread);
-		m_manager.setDefaultMaxPerRoute(20);
+		ConnectingIOReactor ioreactor = new DefaultConnectingIOReactor();
+		m_asyncManager = new PoolingNHttpClientConnectionManager(ioreactor);
+		m_asyncManager.setDefaultMaxPerRoute(20);
+		m_asyncManager.setMaxTotal(500);
 
 		RequestConfig.Builder reqCfgBuilder = RequestConfig.custom();
 		reqCfgBuilder.setSocketTimeout(k_TIMEOUT);
@@ -100,8 +105,9 @@ public class Crawler {
 		reqCfgBuilder.setConnectionRequestTimeout(k_TIMEOUT);
 		reqCfgBuilder.setCookieSpec(CookieSpecs.STANDARD);
 
-		m_client = HttpClients.custom().setConnectionManager(m_manager)
-				.setDefaultRequestConfig(reqCfgBuilder.build()).build();
+		m_asyncClient = HttpAsyncClients.custom()
+				.setDefaultRequestConfig(reqCfgBuilder.build())
+				.setConnectionManager(m_asyncManager).build();
 
 		Spark.port(port);
 
@@ -124,25 +130,49 @@ public class Crawler {
 				m_robotCache);
 	}
 
-	private HttpResponse doRequest(String method, URL url) throws IOException {
+	private void doRequestAsync(String method,
+			URL url,
+			Consumer<HttpResponse> cb) {
 		if (method.equals("HEAD")) {
 			HttpHead req = new HttpHead(url.toString());
 			req.addHeader("User-agent", k_USER_AGENT);
-			HttpResponse response = m_client.execute(req);
+			m_asyncClient.execute(req, new FutureCallback<HttpResponse>() {
 
-			logger.debug(url.toString() + ", rc: "
-					+ response.getStatusLine().getStatusCode());
+				@Override
+				public void failed(Exception e) {
+					logger.info("Exception occured in async request", e);
+				}
 
-			return response;
+				@Override
+				public void completed(HttpResponse resp) {
+					cb.accept(resp);
+				}
+
+				@Override
+				public void cancelled() {
+					// Ignore.
+				}
+			});
 		} else {
 			HttpGet req = new HttpGet(url.toString());
 			req.addHeader("User-agent", k_USER_AGENT);
-			HttpResponse response = m_client.execute(req);
+			m_asyncClient.execute(req, new FutureCallback<HttpResponse>() {
 
-			logger.debug(url.toString() + ", rc: "
-					+ response.getStatusLine().getStatusCode());
+				@Override
+				public void cancelled() {
+					// Ignore
+				}
 
-			return response;
+				@Override
+				public void completed(HttpResponse resp) {
+					cb.accept(resp);
+				}
+
+				@Override
+				public void failed(Exception e) {
+					logger.info("Exception occured in async request", e);
+				}
+			});
 		}
 	}
 
@@ -261,6 +291,110 @@ public class Crawler {
 		logger.info("Started crawler");
 	}
 
+	private void processHeadResponse(URLFrontier.Request req,
+			HttpResponse resp) {
+		String contentType =
+			getCanonicalContentType(resp.getHeaders("content-type"));
+		if (contentType == null || !contentType.endsWith("html")) {
+			logger.debug(req.url.toString() + " is not html: " + contentType);
+			return;
+		}
+
+		RobotInfo info = m_robotCache.getInfoForUrl(req.url);
+
+		int contentLength = getContentLength(resp.getHeaders("content-length"));
+
+		if (contentLength > k_MAX_DOC_SIZE && contentLength < 0) {
+			logger.info(req.url.toString() + " has bad content-length: "
+					+ contentLength);
+			return;
+		}
+
+		if (info == null
+				|| RobotInfoCache.canCrawl(info, req.url, k_USER_AGENT)) {
+			logger.debug("Adding " + req.url.toString() + " for GET.");
+			URLFrontier.Priority prio = computePriority(req.url, resp);
+			long releaseTime = computeReleaseTime(req.url);
+			m_cluster.putUrlThreadPool().submit(() -> {
+				try {
+					m_URLFrontier.put(req.url, prio, releaseTime);
+				} catch (InterruptedException e) {
+					logger.warn("Frontier put interrupted", e);
+				}
+			});
+		}
+
+	}
+
+	private void processGetResponse(URLFrontier.Request req,
+			HttpResponse resp) {
+		byte[] content = null;
+		try {
+			content = EntityUtils.toByteArray(resp.getEntity());
+		} catch (IOException e) {
+			logger.warn("IOException when reading body", e);
+			return;
+		}
+
+		String contentType =
+			getCanonicalContentType(resp.getHeaders("content-type"));
+		if (contentType == null) {
+			contentType = "unknown";
+		}
+		String charset = getCharset(resp.getHeaders("content-type"));
+		if (charset == null) {
+			charset = "unknown";
+		}
+
+		if (!d_contentSeenCache.hasSeen(content)) {
+			// Store into Ddb.
+			DdbDocument ddbDoc = new DdbDocument();
+			S3Link contentLink = d_ddbConnector
+					.createS3Link(DdbConnector.urlToS3LinkKey(req.url));
+			ddbDoc.setContentLink(contentLink);
+			ddbDoc.setContent(content);
+			ddbDoc.setCharset(charset);
+
+			long now = (new Date()).getTime();
+			ddbDoc.setCrawledTime(now);
+			ddbDoc.setUrl(req.url);
+			try {
+				ddbDoc.setUrlAsString(req.url.toString());
+			} catch (MalformedURLException urlException) {
+				logger.warn("Bad url encountered", urlException);
+				return;
+			}
+			ddbDoc.setFingerprint(
+					DdbDocument.computeFingerprint(ddbDoc.getContent()));
+			ddbDoc.setContentType(contentType);
+
+			logger.debug("Stored " + req.url.toString() + " to the database.");
+
+			m_context.incDocsCounter();
+
+			// Extract the URLs
+			String[] urls = URLExtractor.extract(ddbDoc.getContent());
+			HashSet<String> extractedLinks = new HashSet<>();
+			logger.debug("Extracted urls: " + Arrays.toString(urls));
+			for (int i = 0; i < urls.length; ++i) {
+				try {
+					URL resolvedUrl = new URL(req.url, urls[i]);
+					extractedLinks.add(resolvedUrl.toString());
+					m_context.putTask(resolvedUrl);
+				} catch (MalformedURLException e) {
+					// Ignore.
+				}
+			}
+
+			ddbDoc.setLinks(extractedLinks);
+			// new documents have links
+			ddbDoc.setRepaired(true);
+			d_ddbConnector.putDocument(ddbDoc);
+		} else {
+			logger.info("Has already seen: " + req.url.toString());
+		}
+	}
+
 	private void threadLoop() {
 		while (m_context.getDocsCounter() < k_DOC_COUNT) {
 			try {
@@ -273,130 +407,17 @@ public class Crawler {
 					continue;
 				}
 
-				try {
-					logger.debug("Requesting for: " + req.url.toString());
+				logger.debug("Requesting for: " + req.url.toString());
 
-					HttpResponse resp = null;
-					try {
-						resp = doRequest(req.method, req.url);
-					} catch (IOException e) {
-						logger.debug("IOException when requesting: "
-								+ req.url.toString(), e);
-						continue;
-					}
-
-					logger.debug("Finished request: " + req.url.toString());
-
-					if (req.method.equals("HEAD")) {
-						String contentType = getCanonicalContentType(
-								resp.getHeaders("content-type"));
-						if (contentType == null
-								|| !contentType.endsWith("html")) {
-							logger.debug(req.url.toString() + " is not html: "
-									+ contentType);
-							continue;
-						}
-
-						RobotInfo info = m_robotCache.getInfoForUrl(req.url);
-
-						int contentLength =
-							getContentLength(resp.getHeaders("content-length"));
-
-						if (contentLength > k_MAX_DOC_SIZE
-								&& contentLength < 0) {
-							logger.info(req.url.toString()
-									+ " has bad content-length: "
-									+ contentLength);
-							continue;
-						}
-
-						if (info == null || RobotInfoCache.canCrawl(info,
-								req.url,
-								k_USER_AGENT)) {
-							logger.debug("Adding " + req.url.toString()
-									+ " for GET.");
-							URLFrontier.Priority prio =
-								computePriority(req.url, resp);
-							long releaseTime = computeReleaseTime(req.url);
-							m_cluster.putUrlThreadPool().submit(() -> {
-								try {
-									m_URLFrontier.put(req.url,
-											prio,
-											releaseTime);
-								} catch (InterruptedException e) {
-									logger.warn("Frontier put interrupted", e);
-								}
-							});
-						}
-					} else if (req.method.equals("GET")) {
-
-						byte[] content =
-							EntityUtils.toByteArray(resp.getEntity());
-
-						String contentType = getCanonicalContentType(
-								resp.getHeaders("content-type"));
-						if (contentType == null) {
-							contentType = "unknown";
-						}
-						String charset =
-							getCharset(resp.getHeaders("content-type"));
-						if (charset == null) {
-							charset = "unknown";
-						}
-
-						if (!d_contentSeenCache.hasSeen(content)) {
-							// Store into Ddb.
-							DdbDocument ddbDoc = new DdbDocument();
-							S3Link contentLink = d_ddbConnector.createS3Link(
-									DdbConnector.urlToS3LinkKey(req.url));
-							ddbDoc.setContentLink(contentLink);
-							ddbDoc.setContent(content);
-							ddbDoc.setCharset(charset);
-
-							long now = (new Date()).getTime();
-							ddbDoc.setCrawledTime(now);
-							ddbDoc.setUrl(req.url);
-							ddbDoc.setUrlAsString(req.url.toString());
-							ddbDoc.setFingerprint(DdbDocument
-									.computeFingerprint(ddbDoc.getContent()));
-							ddbDoc.setContentType(contentType);
-
-							logger.debug("Stored " + req.url.toString()
-									+ " to the database.");
-
-							m_context.incDocsCounter();
-
-							// Extract the URLs
-							String[] urls =
-								URLExtractor.extract(ddbDoc.getContent());
-							HashSet<String> extractedLinks = new HashSet<>();
-							logger.debug(
-									"Extracted urls: " + Arrays.toString(urls));
-							for (int i = 0; i < urls.length; ++i) {
-								try {
-									URL resolvedUrl = new URL(req.url, urls[i]);
-									extractedLinks.add(resolvedUrl.toString());
-									m_context.putTask(resolvedUrl);
-								} catch (MalformedURLException e) {
-									// Ignore.
-								}
-							}
-
-							ddbDoc.setLinks(extractedLinks);
-							// new documents have links
-							ddbDoc.setRepaired(true);
-							d_ddbConnector.putDocument(ddbDoc);
-						} else {
-							logger.info(
-									"Has already seen: " + req.url.toString());
-						}
-					}
-				} catch (IOException e) {
-					logger.warn(
-							"IOException when requesting " + req.url.toString(),
-							e);
+				if (req.method.equals("HEAD")) {
+					doRequestAsync(req.method, req.url, (resp) -> {
+						processHeadResponse(req, resp);
+					});
+				} else {
+					doRequestAsync(req.method, req.url, (resp) -> {
+						processGetResponse(req, resp);
+					});
 				}
-
 			} catch (InterruptedException e) {
 				logger.info("Interrupted!", e);
 			} catch (Throwable e) {
