215f5c5a9bfcc01e1d7c208543f3420394667ed1
shrekshao <shrekshao@gmail.com>
2017-04-27 21:57:13 -0400

    delete database/ in .gitignore

--- src/com/tinysearchengine/indexer/database/DdbKeyword.java
+++ src/com/tinysearchengine/indexer/database/DdbKeyword.java
@@ -1,19 +1,21 @@
 package com.tinysearchengine.indexer.database;
 
-import java.net.MalformedURLException;
-import java.net.URL;
+import java.util.Set;
 
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBAttribute;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBHashKey;
-import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBIgnore;
+//import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBIgnore;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
 
 @DynamoDBTable(tableName = "KeywordTable")
 public class DdbKeyword {
 
-	private String d_word;
+	private String d_word;	// after stemmed
 	private int d_globalCount;
-	private float d_idf;		// log( N/n )
+	private float d_idf;		// log( N/n ), default set to 1
+	
+	private Set<String> d_docids;	// url
+	
 
 	@DynamoDBHashKey(attributeName = "word")
 	public String getKeyword() {
@@ -27,16 +29,29 @@ public class DdbKeyword {
 	public int getGlobalCount() {
 		return d_globalCount;
 	}
-	public void setContent(int count) {
+	public void setGlobalCount(int count) {
 		d_globalCount = count;
 	}
 
 	@DynamoDBAttribute(attributeName = "idf")
-	public float getContentType() {
+	public float getIdf() {
 		return d_idf;
 	}
-	public void setContentType(float idf) {
+	public void setIdf(float idf) {
 		d_idf = idf;
 	}
 
+	@DynamoDBAttribute(attributeName = "docs")
+	public Set<String> getDocIds() {
+		return d_docids;
+	}
+
+	public void setDocIds(Set<String> docids) {
+		if (docids == null || docids.isEmpty()) {
+			d_docids = null;
+		} else {
+			d_docids = docids;
+		}
+	}
+	
 }
