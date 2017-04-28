e10b8a76b8d7f0c4f3cc107ea135ccf9027535ed
Xueyin <xueyinw@seas.upenn.edu>
2017-04-28 18:51:21 -0400

    PageRank: Add finalize reducer

--- src/com/tinysearchengine/pagerank/PageRankFinalizeReducer.java
+++ src/com/tinysearchengine/pagerank/PageRankFinalizeReducer.java
@@ -1,5 +1,16 @@
 package com.tinysearchengine.pagerank;
 
-public class PageRankFinalizeReducer {
+import java.io.IOException;
 
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+public class PageRankFinalizeReducer extends Reducer<Text,Text,Text,Text> {
+	@Override 
+	public void reduce(Text key ,Iterable<Text> values,Context context) throws IOException, InterruptedException {
+		for(Text value : values) {
+			context.write(key, value); //we need to write the url & score
+			return;
+		}
+	}	
 }
