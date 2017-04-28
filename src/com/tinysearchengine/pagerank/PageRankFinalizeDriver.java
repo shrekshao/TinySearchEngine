e3e05cf676c4a47b71dde0982a9fc845d6cb327e
Xueyin <xueyinw@seas.upenn.edu>
2017-04-28 19:09:12 -0400

    PageRank: Finish Finalization Driver, need to test

--- src/com/tinysearchengine/pagerank/PageRankFinalizeDriver.java
+++ src/com/tinysearchengine/pagerank/PageRankFinalizeDriver.java
@@ -1,5 +1,32 @@
 package com.tinysearchengine.pagerank;
 
-public class PageRankFinalizeDriver {
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.Tool;
 
+public class PageRankFinalizeDriver extends Configured implements Tool{
+	@Override
+	public int run(String[] arg0) throws Exception {
+		// TODO Auto-generated method stub
+		Job job = Job.getInstance(); 
+	    job.setJobName("PageRankFinalStep");
+	    job.setJarByClass(PageRankFinalizeDriver.class);
+	     
+	    job.setMapperClass(PageRankFinalizeMapper.class); //Set Mapper Class
+	    job.setReducerClass(PageRankFinalizeReducer.class); //Set Reducer Class 
+	    
+	    job.setMapOutputKeyClass(Text.class); //Set Mapper Output
+	    job.setMapOutputValueClass(Text.class);
+	    job.setOutputKeyClass(Text.class); //Set Reducer Output
+	    job.setOutputValueClass(Text.class);
+	  
+	    FileInputFormat.setInputPaths(job, new Path(arg0[0]));
+	    FileOutputFormat.setOutputPath(job, new Path(arg0[1]));
+	    
+		return job.waitForCompletion(true) ? 0 : 1;
+	}
 }
