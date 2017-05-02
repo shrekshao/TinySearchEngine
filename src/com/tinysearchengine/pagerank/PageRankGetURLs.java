c52ac443b4a20f52efd8a961cda90418296d1db7
xueyin wan <xueyinw@seas.upenn.edu>
2017-05-02 09:13:09 -0400

    PageRank: faster

--- src/com/tinysearchengine/pagerank/PageRankGetURLs.java
+++ src/com/tinysearchengine/pagerank/PageRankGetURLs.java
@@ -6,6 +6,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -21,21 +22,27 @@ public class PageRankGetURLs {
 	public static void main(String args[]) {
 		docs = d_connector.getAllDocumentsLazily();
 		Iterator<DdbDocument> docIt = docs.iterator();
-		int counter = 500;
+		int sum = 0;
+		//int counter = 50000;
 		try {
 			//File statText = new File("C:/Users/xueyin/Documents/GitHub/TinySearchEngine/pagerankinput/myinput1.txt");
 			File statText = new File("/Users/owner/TinySearchEngine/pagerankinput/macinput.txt");
 			FileOutputStream is = new FileOutputStream(statText);
 			OutputStreamWriter osw = new OutputStreamWriter(is);    
 			Writer w = new BufferedWriter(osw);
-			while (counter > 0 && docIt.hasNext()) {
+			while (docIt.hasNext()) {
 				DdbDocument doc = docIt.next();
 				w.write(doc.getUrlAsString() + "	Score|1.0");
-				if (doc.getLinks() == null) { 
+				if (doc.getLinks() == null) {  
 					byte[] curByte = doc.getContent();
+					if(curByte == null) {
+						sum++;
+						continue;
+					}
 					String[] links = URLExtractor.extract(curByte);
 					for(String link: links) {
 						if(link.startsWith("https") || link.startsWith("http")) {
+							new URL(link, parsedUrlStr).toString();
 							if(link.contains(" ")) {
 								link = link.replaceAll(" ", "%20");
 							}
@@ -53,7 +60,11 @@ public class PageRankGetURLs {
 					}
 				}
 				w.write("\r\n");
-				counter--;
+				//counter--;
+				sum++;
+				if (sum % 500 == 0) {
+					System.out.println("Have already written :" + sum/500 + " * 500 of files");
+				}
 			}
 			w.close();
 			System.out.println("PageRank Test File Formation Finished :)");
@@ -61,12 +72,4 @@ public class PageRankGetURLs {
         	e.printStackTrace();
         }
 	}
-	
-	
-	
-	
-	
-	
-	
-
 }
