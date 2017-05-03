c4b9899f6cbacb49c8d78ed92e73ad9a2b34ccd9
immiao <miaokaixiang@gmail.com>
2017-05-03 00:50:15 -0400

    pretending to work hard till late...

---
+++ src/com/tinysearchengine/searchengine/MainPageServlet.java
@@ -0,0 +1,12 @@
+package com.tinysearchengine.searchengine;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MainPageServlet extends HttpServlet {
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response) {
+		
+	}
+}
