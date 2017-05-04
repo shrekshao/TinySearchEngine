aa28dfe8fa157a9d3eeb12ee412eed6165db29bb
hengchu <hengchu.zhang@gmail.com>
2017-05-04 01:06:06 -0400

    SearchEngineServlet: reorganized the servlet build.xml

--- src/com/tinysearchengine/searchengine/servlet/SearchServlet.java
+++ src/com/tinysearchengine/searchengine/servlet/SearchServlet.java
@@ -1,21 +1,24 @@
 package com.tinysearchengine.searchengine.servlet;
 
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
-import java.io.PrintWriter;
+import java.io.InputStreamReader;
+import java.util.Date;
+import java.util.HashMap;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.io.IOUtils;
+
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+
 public class SearchServlet extends HttpServlet {
 	@Override
-	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-		PrintWriter out = response.getWriter();
-		out.println("Result");
-		out.close();
+	public void doGet(HttpServletRequest request,
+			HttpServletResponse response) {
 	}
 }
