3cd53fda9cce4d90a5f19c54bf190dfc997832c2
hengchu <hengchu.zhang@gmail.com>
2017-05-04 18:01:37 -0400

    Servlet: adding amazon integration

--- src/com/tinysearchengine/searchengine/servlet/SearchServlet.java
+++ src/com/tinysearchengine/searchengine/servlet/SearchServlet.java
@@ -5,15 +5,17 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.commons.io.IOUtils;
+
+import com.tinysearchengine.searchengine.othersites.AmazonItemResult;
+import com.tinysearchengine.searchengine.othersites.RequestToOtherSites;
 
 import freemarker.template.Configuration;
 import freemarker.template.Template;
@@ -21,6 +23,8 @@ import freemarker.template.TemplateException;
 import freemarker.template.TemplateExceptionHandler;
 
 public class SearchServlet extends HttpServlet {
+
+	final int k_MAX_AMAZON_RESULTS = 15;
 
 	Configuration d_templateConfiguration;
 
@@ -34,11 +38,11 @@ public class SearchServlet extends HttpServlet {
 		public void setTitle(String title) {
 			d_title = title;
 		}
-		
+
 		public String getTitle() {
 			return d_title;
 		}
-		
+
 		public void setUrl(String url) {
 			d_url = url;
 		}
@@ -55,6 +59,55 @@ public class SearchServlet extends HttpServlet {
 			return d_summary;
 		}
 
+	}
+
+	public class ThirdPartyResult {
+		private String d_itemUrl;
+		private String d_imgUrl;
+		private String d_title;
+		private String d_price;
+
+		public ThirdPartyResult(AmazonItemResult amzr) {
+			setItemUrl(amzr.itemUrl);
+			setImageUrl(amzr.imgUrl);
+			setTitle(amzr.title);
+			setPrice(amzr.price);
+		}
+
+		public void setItemUrl(String url) {
+			d_itemUrl = url;
+		}
+
+		public String getItemUrl() {
+			return d_itemUrl;
+		}
+
+		public void setImageUrl(String url) {
+			d_imgUrl = url;
+		}
+
+		public String getImageUrl() {
+			return d_imgUrl;
+		}
+
+		public void setTitle(String title) {
+			if (title.length() > 100) {
+				title = title.substring(0, 97) + "...";
+			}
+			d_title = title;
+		}
+
+		public String getTitle() {
+			return d_title;
+		}
+
+		public void setPrice(String p) {
+			d_price = p;
+		}
+
+		public String getPrice() {
+			return d_price;
+		}
 	}
 
 	public void init() throws ServletException {
@@ -81,23 +134,42 @@ public class SearchServlet extends HttpServlet {
 	public void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws IOException, ServletException {
 		HashMap<String, Object> root = new HashMap<>();
+
+		String queryTerm = request.getParameter("query");
+		root.put("query", queryTerm);
+
+		// Put the search results in
+		ArrayList<SearchResult> results = new ArrayList<>();
+
+		for (int i = 0; i < 10; ++i) {
+			SearchResult r = new SearchResult();
+			r.setTitle("This is foobar" + i);
+			r.setUrl("http://www.foobar" + i + ".com");
+			r.setSummary(String.join("",
+					Collections.nCopies(20, "This is the content of foobar.")));
+			results.add(r);
+		}
+
+		root.put("searchResults", results);
+
+		// Put the amazon results in
+		List<ThirdPartyResult> thirdPartyResults = new ArrayList<>();
+		final List<ThirdPartyResult> collectedResults = thirdPartyResults;
+		RequestToOtherSites.getAmazonResult(queryTerm).forEach((item) -> {
+			collectedResults.add(new ThirdPartyResult(item));
+		});
+		if (thirdPartyResults.size() > k_MAX_AMAZON_RESULTS) {
+			thirdPartyResults =
+				thirdPartyResults.subList(0, k_MAX_AMAZON_RESULTS);
+		}
+
+		root.put("thirdPartyResults", thirdPartyResults);
+
 		try {
-			ArrayList<SearchResult> results = new ArrayList<>();
-
-			for (int i = 0; i < 10; ++i) {
-				SearchResult r = new SearchResult();
-				r.setTitle("This is foobar" + i);
-				r.setUrl("http://www.foobar" + i + ".com");
-				r.setSummary(String.join("",
-						Collections.nCopies(20,
-								"This is the content of foobar.")));
-				results.add(r);
-			}
-
-			root.put("searchResults", results);
 			d_searchResultTemplate.process(root, response.getWriter());
 		} catch (TemplateException e) {
 			throw new ServletException(e);
 		}
+
 	}
 }
