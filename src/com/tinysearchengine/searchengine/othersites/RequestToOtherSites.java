d9e40ca16028b19db012430e0a1c822f693ec6d1
immiao <miaokaixiang@gmail.com>
2017-05-04 18:51:19 -0400

    Search Engine: added ebay requestor

--- src/com/tinysearchengine/searchengine/othersites/RequestToOtherSites.java
+++ src/com/tinysearchengine/searchengine/othersites/RequestToOtherSites.java
@@ -1,6 +1,8 @@
 package com.tinysearchengine.searchengine.othersites;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -18,6 +20,9 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 public class RequestToOtherSites {
+	
+	static CloseableHttpClient httpClient = HttpClients.createDefault();
+	
 	static public ArrayList<AmazonItemResult> getAmazonResult(String[] keywords)
 			throws ClientProtocolException, IOException {
 		return getAmazonResult(Arrays.toString(keywords));
@@ -28,12 +33,16 @@ public class RequestToOtherSites {
 		keyword = URLEncoder.encode(keyword, "UTF-8");
 		final HttpGet request = new HttpGet(
 				"https://www.amazon.com/s/ref=nb_sb_ss_c_1_3?url=search-alias%3Daps&field-keywords=" + keyword);
-		CloseableHttpClient httpClient = HttpClients.createDefault();
+		
 		CloseableHttpResponse httpResponse = httpClient.execute(request);
 		HttpEntity entity = httpResponse.getEntity();
 		String content = EntityUtils.toString(entity);
 
 		Document doc = Jsoup.parse(content);
+		
+//		PrintWriter writer = new PrintWriter("response.html", "UTF-8");
+//		writer.println(content);
+//		writer.close();
 
 		int total = 0;
 		ArrayList<AmazonItemResult> itemList = new ArrayList<AmazonItemResult>();
@@ -73,10 +82,63 @@ public class RequestToOtherSites {
 				Element itemFractional = tmp2.get(0);
 				item.price = itemCurrency.text() + itemWhole.text() + "." + itemFractional.text();
 			}
+
 			itemList.add(item);
 			total++;
 		}
 
+		return itemList;
+	}
+	
+	static public ArrayList<EbayItemResult> getEbayResult(String[] keywords)
+			throws ClientProtocolException, IOException {
+		return getEbayResult(Arrays.toString(keywords));
+	}
+	
+	static public ArrayList<EbayItemResult> getEbayResult(String keyword) throws ClientProtocolException, IOException {
+		keyword = URLEncoder.encode(keyword, "UTF-8");
+		final HttpGet request = new HttpGet(
+				"http://www.ebay.com/sch/i.html?_nkw=" + keyword);
+		
+		CloseableHttpResponse httpResponse = httpClient.execute(request);
+		HttpEntity entity = httpResponse.getEntity();
+		String content = EntityUtils.toString(entity);
+
+		Document doc = Jsoup.parse(content);
+		
+//		PrintWriter writer = new PrintWriter("ebay2.html", "UTF-8");
+//		writer.println(content);
+//		writer.close();
+		
+		ArrayList<EbayItemResult> itemList = new ArrayList<EbayItemResult>();
+		Element ul = doc.getElementById("ListViewInner");
+		if (ul != null) {
+			Elements lis = ul.select("li[id]").select("[_sp]").select("[class]").select("[listingid]").select("[r]");
+			for (Element e : lis) {
+				EbayItemResult item = new EbayItemResult();
+				Elements lvtitle = e.select("h3[class=\"lvtitle\"]").select("a");
+				if (!lvtitle.isEmpty()) {
+					item.title = lvtitle.text();
+					item.itemUrl = lvtitle.attr("href");
+				}
+				Elements img = e.select("img[src]");
+				//Elements img = e.select("div[class=\"lvpicinner full-width picW\"]").select("img[src]").select("[class=\"img\"]").select("[alt]");
+				if (!img.isEmpty()) {
+					// sometimes the image url is in src attribute and sometimes is in imgurl
+					String imgurl0 = img.attr("src");
+					String imgurl1 = img.attr("imgurl");
+					if (!imgurl0.isEmpty() && !imgurl0.endsWith(".gif"))
+						item.imgUrl = imgurl0;
+					if (!imgurl1.isEmpty() && !imgurl1.endsWith(".gif"))
+						item.imgUrl = imgurl1;
+				}
+				Elements price = e.select("li[class=\"lvprice prc\"]");
+				if (!price.isEmpty()) {
+					item.price = price.text();
+				}
+				itemList.add(item);
+			}
+		}
 		return itemList;
 	}
 }
--- src/com/tinysearchengine/searchengine/othersites/test/TestRequestToOtherSites.java
+++ src/com/tinysearchengine/searchengine/othersites/test/TestRequestToOtherSites.java
@@ -7,14 +7,30 @@ import java.util.ArrayList;
 import org.apache.http.client.ClientProtocolException;
 
 import com.tinysearchengine.searchengine.othersites.AmazonItemResult;
+import com.tinysearchengine.searchengine.othersites.EbayItemResult;
 import com.tinysearchengine.searchengine.othersites.RequestToOtherSites;
 
 public class TestRequestToOtherSites {
 	public static void main(String args[]) throws ClientProtocolException, IOException {
-		ArrayList<AmazonItemResult> result = RequestToOtherSites.getAmazonResult("Head Dryer");
+		//TestAmazon();
+		TestEbay();
+	}
+	
+	static void TestAmazon() throws ClientProtocolException, IOException {
+		ArrayList<AmazonItemResult> result = RequestToOtherSites.getAmazonResult("laptop");
 		
 		int c = 0;
 		for (AmazonItemResult item : result) {
+			System.out.println("Item " + (c++) + ":");
+			item.print();
+		}
+	}
+	
+	static void TestEbay() throws ClientProtocolException, IOException { 
+		ArrayList<EbayItemResult> result = RequestToOtherSites.getEbayResult("laptop apple mac");
+		
+		int c = 0;
+		for (EbayItemResult item : result) {
 			System.out.println("Item " + (c++) + ":");
 			item.print();
 		}
