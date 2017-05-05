569dce716b39ff9216ed20bc24e331dd2bb78947
immiao <miaokaixiang@gmail.com>
2017-05-04 20:16:16 -0400

    Search Engine: added User-Agent header for Ebay request

--- src/com/tinysearchengine/searchengine/othersites/RequestToOtherSites.java
+++ src/com/tinysearchengine/searchengine/othersites/RequestToOtherSites.java
@@ -100,7 +100,7 @@ public class RequestToOtherSites {
 		keyword = URLEncoder.encode(keyword, "UTF-8");
 		final HttpGet request = new HttpGet(
 				"http://www.ebay.com/sch/i.html?_nkw=" + keyword);
-		
+		request.setHeader("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.81 Safari/537.36");
 		CloseableHttpResponse httpResponse = httpClient.execute(request);
 		HttpEntity entity = httpResponse.getEntity();
 		String content = EntityUtils.toString(entity);
