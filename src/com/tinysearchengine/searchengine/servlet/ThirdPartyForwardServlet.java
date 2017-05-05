3bf279b0fb8ac25ad0922358b61b745e418105b2
immiao <miaokaixiang@gmail.com>
2017-05-05 15:55:19 -0400

    Servlet: added Ebay logo

--- src/com/tinysearchengine/searchengine/servlet/ThirdPartyForwardServlet.java
+++ src/com/tinysearchengine/searchengine/servlet/ThirdPartyForwardServlet.java
@@ -11,37 +11,15 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.tinysearchengine.searchengine.othersites.AmazonItemResult;
+import com.tinysearchengine.searchengine.othersites.EbayItemResult;
 import com.tinysearchengine.searchengine.othersites.RequestToOtherSites;
 
 public class ThirdPartyForwardServlet extends HttpServlet {
-//	private String encodeQuote(String content) {
-//		return content.replace("\"", "\\\"").replace("\\", "\\\\").replace("\t", "\\t").replace("\n", "\\n");
-//	}
 	@Override
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
 		String name = request.getParameter("name");
 		String query = request.getParameter("query");
-		//System.out.println(encodeQuote("http://cnm.com/\"helo\""));
 		if (name.equals("amazon")) {
-			// my own json converter
-//			ArrayList<AmazonItemResult> result = RequestToOtherSites.getAmazonResult(query);
-//			PrintWriter writer = response.getWriter();
-//			writer.write("[");
-//			int length = result.size();
-//			for (int i = 0; i < length; i++) {
-//				writer.write("{");
-//				writer.write("\"price\":" + "\"" + encodeQuote(result.get(i).price) + "\",");
-//				writer.write("\"itemUrl\":" + "\"" + encodeQuote(result.get(i).itemUrl) + "\",");
-//				writer.write("\"imgUrl\":" + "\"" + encodeQuote(result.get(i).imgUrl) + "\",");
-//				writer.write("\"title\":" + "\"" + encodeQuote(result.get(i).title) + "\"");
-//				writer.write("}");
-//				if (i != length - 1)
-//					writer.write(",");
-//			}	
-//			writer.write("]");
-//			writer.close();
-			
-			// use ObjectMapper instead
 			ArrayList<AmazonItemResult> result = RequestToOtherSites.getAmazonResult(query);
 			PrintWriter writer = response.getWriter();
 			
@@ -50,7 +28,13 @@ public class ThirdPartyForwardServlet extends HttpServlet {
 			writer.write(jsonContent);
 			writer.close();
 		} else if (name.equals("ebay")) {
+			ArrayList<EbayItemResult> result = RequestToOtherSites.getEbayResult(query);
+			PrintWriter writer = response.getWriter();
 			
+			ObjectMapper mapper = new ObjectMapper();
+			String jsonContent = mapper.writeValueAsString(result);
+			writer.write(jsonContent);
+			writer.close();
 		}
 	}
 }
--- src/com/tinysearchengine/searchengine/servlet/thirdpartyrequest.js
+++ src/com/tinysearchengine/searchengine/servlet/thirdpartyrequest.js
@@ -56,7 +56,55 @@ window.onload = function() {
 	}
 
 	if (document.getElementById("ebay-checkbox").getAttribute("checked") != null) {
+		var xhr = new XMLHttpRequest();
+		xhr.onreadystatechange = function() {
+			if (this.readyState == 4 && this.status == 200) {
+				var itemInfos = JSON.parse(this.responseText);
+				var resultContainer = document
+						.getElementById("thirdparty-result-container");
 
+				for (var i = 0; i < itemInfos.length; i++) {
+					var itemInfo = itemInfos[i];
+					console.log(itemInfo);
+					var itemDiv = document.createElement("div");
+					itemDiv.setAttribute("class", "thirdparty-result-item");
+
+					// add image
+					var itemImg = document.createElement("img");
+					itemImg.setAttribute("src", itemInfo.imgUrl);
+					itemDiv.appendChild(itemImg);
+
+					// add title and url
+					var h4 = document.createElement("h4");
+					var itemUrl = document.createElement("a");
+					itemUrl.setAttribute("href", itemInfo.itemUrl);
+					itemUrl.textContent = itemInfo.title;
+					h4.appendChild(itemUrl);
+					itemDiv.appendChild(h4);
+
+					// add price
+					var priceDiv = document.createElement("div");
+					
+					var logo = document.createElement("img");
+					logo.setAttribute("src", "/img/ebay-logo.jpg");
+					priceDiv.appendChild(logo);
+					
+					var price = document.createElement("p");
+					price.textContent = itemInfo.price;
+					priceDiv.appendChild(price);
+					
+					itemDiv.appendChild(priceDiv);
+					
+					// add all things to item div
+					resultContainer.appendChild(itemDiv);
+				}
+			}
+		}
+		var url = location.protocol + '//' + location.host
+				+ "/thirdparty?name=ebay&&query=" + query;
+		console.log(url);
+		xhr.open('GET', url, true);
+		xhr.send();
 	}
 
 }
